85bf336192b6eb32d2df898feaf33485eeaa0106
Leandro Matosas <96553064+leandromatc@users.noreply.github.com>
2024-06-26 16:19:57 -0300

    bg animation

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import AudioPlayer from "./components/AudioPlayer";
 
 function App() {
   return (
-    <div className="w-full h-screen flex justify-center items-center bg-neutral-100 flex-col gap-10">
-      <div>
-        <h1 className="font-semibold uppercase text-6xl text-neutral-800 text-center">
-          ¡Nos estamos renovando!
-        </h1>
-        <p className="text-neutral-800 text-center mt-4">
-          Mientras tanto, podes escucharnos desde aquí.
-        </p>
+    <div className="w-full h-screen flex justify-center items-center bg-neutral-100 flex-col gap-10 relative">
+      <div className="absolute w-[500px] h-[500px] rounded-full bg-red-100 animate-blob mix-blend-multiply filter blur-2xl"></div>
+      <div className="z-10 flex gap-10 flex-col">
+        <div>
+          <h1 className="font-semibold uppercase text-6xl text-neutral-800 text-center">
+            ¡Nos estamos renovando!
+          </h1>
+          <p className="text-neutral-800 text-center mt-4">
+            Mientras tanto, podes escucharnos desde aquí.
+          </p>
+        </div>
+        <AudioPlayer />
       </div>
-      <AudioPlayer />
     </div>
   );
 }
