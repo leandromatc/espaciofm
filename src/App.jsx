91e524f7fac9f35a8688eb74283f762b0146f5a0
Leandro Matosas <lnmatosas@hotmail.com>
2024-06-27 11:37:03 -0300

    Responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import logo from "./assets/logo.webp";
 
 function App() {
   return (
-    <div className="w-full h-screen flex justify-center items-center flex-col gap-10 relative">
+    <div className="w-full h-screen flex justify-center items-center flex-col gap-10 relative p-10">
       <div className="absolute w-[500px] h-[500px] rounded-full bg-red-100 animate-blob mix-blend-multiply filter blur-2xl"></div>
       <picture className="max-w-[540px]">
         <img src={logo} alt="Logo de Espacio Sport 91.5" />
       </picture>
       <div className="z-10 flex gap-10 flex-col">
         <div>
-          <h1 className="font-semibold uppercase text-6xl text-neutral-800 text-center">
+          <h1 className="font-bold uppercase md:text-6xl text-neutral-800 text-center text-4xl tracking-tight">
             ¡Nos estamos renovando!
           </h1>
           <p className="text-neutral-800 text-center mt-4">
