0a48cdd63ba45b1ab566c1f695254b362deba563
Leandro Matosas <lnmatosas@hotmail.com>
2024-06-27 11:34:04 -0300

    Logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import AudioPlayer from "./components/AudioPlayer";
+import logo from "./assets/logo.webp";
 
 function App() {
   return (
-    <div className="w-full h-screen flex justify-center items-center bg-neutral-100 flex-col gap-10 relative">
+    <div className="w-full h-screen flex justify-center items-center flex-col gap-10 relative">
       <div className="absolute w-[500px] h-[500px] rounded-full bg-red-100 animate-blob mix-blend-multiply filter blur-2xl"></div>
+      <picture className="max-w-[540px]">
+        <img src={logo} alt="Logo de Espacio Sport 91.5" />
+      </picture>
       <div className="z-10 flex gap-10 flex-col">
         <div>
           <h1 className="font-semibold uppercase text-6xl text-neutral-800 text-center">
