bbdf622e9a5cec28c471f10c11a62ea5d50687f4
Leandro Matosas <96553064+leandromatc@users.noreply.github.com>
2024-12-30 19:56:33 -0300

    Hero responsive

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,9 +5,9 @@ import Ping from "./Ping";
 
 const Hero = () => {
   return (
-    <section className="flex min-h-screen w-full flex-col px-5" id="hero">
+    <section className="flex w-full flex-col px-5 md:min-h-screen" id="hero">
       <Navbar />
-      <div className="flex flex-grow flex-col items-center justify-center gap-10">
+      <div className="flex flex-col items-center justify-center gap-10 py-20 md:flex-grow md:py-0">
         <div className="flex flex-col items-center gap-4">
           <span className="flex items-center gap-2 rounded-full bg-neutral-900 px-2 py-1 text-xs ring-1 ring-inset ring-neutral-800">
             <Ping /> Estamos en vivo
