31478659701a4c9975f6c144d3fe2297e6d46a40
Leandro Matosas <96553064+leandromatc@users.noreply.github.com>
2024-12-30 19:31:06 -0300

    Actualizados programas y about-us

--- components/About.tsx
+++ components/About.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const About = () => {
   return (
-    <section className='py-10 px-5 flex flex-col gap-5 items-center'>
-      <h3 className='text-5xl'>Sobre Nosotros</h3>
-      <p className='text-center max-w-2xl text-neutral-100'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni nobis
-        inventore atque aperiam ipsa deleniti placeat quidem! Minima doloremque
-        tempore at numquam, molestiae, voluptas iste quisquam excepturi quos
-        asperiores aspernatur.
+    <section className="flex flex-col items-center gap-5 px-5 py-10">
+      <h3 className="text-5xl">Sobre Nosotros</h3>
+      <p className="max-w-2xl text-center text-neutral-100">
+        Estación que inició su actividad en 1999, llegando a convertirse en uno
+        de los mejores medio de comunicación de la comunidad uruguaya de
+        Mercedes. Ofrece noticias al día, segmentos deportivos, música y mucho
+        más.
       </p>
     </section>
   );
--- lib/programs.ts
+++ lib/programs.ts
@@ -1,33 +1,9 @@
 // programs.ts
 export const programs = [
   {
-    name: "Morning Show",
-    days: ["lunes", "martes", "miércoles", "jueves", "viernes"],
-    startTime: "08:00",
-    endTime: "10:00",
-  },
-  {
-    name: "Lunch Break Hits",
-    days: ["lunes", "martes", "miércoles", "jueves", "viernes"],
-    startTime: "12:00",
-    endTime: "14:00",
-  },
-  {
     name: "Más que deportes",
     days: ["lunes", "martes", "miércoles", "jueves", "viernes"],
     startTime: "16:00",
     endTime: "19:00",
-  },
-  {
-    name: "Evening Relax",
-    days: ["lunes", "martes", "miércoles", "jueves", "viernes"],
-    startTime: "18:00",
-    endTime: "20:00",
-  },
-  {
-    name: "Weekend Vibes",
-    days: ["sábado", "domingo"],
-    startTime: "10:00",
-    endTime: "12:00",
   },
 ];
