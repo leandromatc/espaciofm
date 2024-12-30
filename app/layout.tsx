1bd5f891376e4b5e16e42c540a4cd89b628990a4
Leandro Matosas <96553064+leandromatc@users.noreply.github.com>
2024-12-30 19:45:09 -0300

    Meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,40 +28,40 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='es' className='dark'>
+    <html lang="es" className="dark">
       <head>
-        <link rel='icon' href='/favicon.svg' />
+        <link rel="icon" href="/favicon.svg" />
         <link
-          rel='apple-touch-icon'
-          sizes='180x180'
-          href='/apple-touch-icon.png'
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
         />
         <link
-          rel='icon'
-          type='image/png'
-          sizes='32x32'
-          href='/favicon-32x32.png'
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
         />
         <link
-          rel='icon'
-          type='image/png'
-          sizes='16x16'
-          href='/favicon-16x16.png'
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
         />
         <link
-          rel='icon'
-          type='image/png'
-          sizes='192x192'
-          href='/images/android-chrome-192x192.png'
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-chrome-192x192.png"
         />
 
         <link
-          rel='icon'
-          type='image/png'
-          sizes='512x512'
-          href='/images/android-chrome-512x512.png'
+          rel="icon"
+          type="image/png"
+          sizes="512x512"
+          href="/android-chrome-512x512.png"
         />
-        <meta name='theme-color' content='#ffffff' />
+        <meta name="theme-color" content="#ffffff" />
       </head>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
