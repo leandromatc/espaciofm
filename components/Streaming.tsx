d059184e74cab6dbdb27a5febd46a834cebb5c27
Leandro Matosas <96553064+leandromatc@users.noreply.github.com>
2025-01-01 15:51:12 -0300

    Add link prompt to Streaming component

--- components/Streaming.tsx
+++ components/Streaming.tsx
@@ -4,7 +4,11 @@ import React from "react";
 const Streaming = () => {
   return (
     <section className="px-5 py-10">
-      <Link href={"https://cv10.plag.tv"} target="_blank">
+      <Link
+        href={"https://cv10.plag.tv"}
+        target="_blank"
+        aria-label="Streaming de CV10"
+      >
         <div className="grid h-full w-full cursor-pointer grid-cols-1 rounded-xl bg-gradient-to-r from-red-700 to-transparent p-10 md:grid-cols-2">
           <div className="col-span-1">
             <h5 className="max-w-xl text-5xl font-semibold">
@@ -13,6 +17,7 @@ const Streaming = () => {
             <p className="my-2">
               Disponible para todo el mundo exceptuando Soriano y Río Negro
             </p>
+            <p className="text-xs underline">Click para ir a la web.</p>
           </div>
         </div>
       </Link>
