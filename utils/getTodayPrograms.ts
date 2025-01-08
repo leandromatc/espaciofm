890b99b7fcd75d7550df3ccbbdd80e1f0f1e9526
Leandro Matosas <96553064+leandromatc@users.noreply.github.com>
2025-01-08 16:48:45 -0300

    Ordenar por hora

--- utils/getTodayPrograms.ts
+++ utils/getTodayPrograms.ts
@@ -4,8 +4,8 @@ export const getTodayPrograms = async () => {
   const now = new Date();
   const currentDay = now.toLocaleString("es-UY", { weekday: "long" });
   const programs = await fetchPrograms();
-  const todayPrograms = programs.filter((program) =>
-    program.days.includes(currentDay),
-  );
+  const todayPrograms = programs
+    .filter((program) => program.days.includes(currentDay))
+    .sort((a, b) => a.start_time.localeCompare(b.start_time));
   return todayPrograms;
 };
